6bedf45696b4509029d9d2a973251098025ad44d
DrishyaChhetri <brewshell@BrewShell.local>
2025-02-22 12:05:39 +0545

    setup: ant design has been setup for modern ui look

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React from "react";
+import { Button } from "antd";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h1>Hello, Ant Design!</h1>
+      <Button type="primary">Click Me</Button>
     </div>
   );
-}
+};
 
 export default App;
